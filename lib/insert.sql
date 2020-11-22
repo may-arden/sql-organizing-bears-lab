55fc9ad64c960589f74324ecb0cddd57e22c1b35
may arden <mayard3n@gmail.com>
2020-11-22 14:16:27 -0800

    commit tree 4fbb1e3ce2e87e82800c994a0fbdaffc72fb210c

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 2, "M", "Brown", "friendly", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 3, "M", "white", "curious", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 4, "F", "Brown", "mild", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 5, "M", "black", "cuddly", 1);
+INSERT INTO bears VALUES (5, "Melissa", 6, "F", "Brown", "adventerous", 0);
+INSERT INTO bears VALUES (6, "Grinch", 7, "M", "Brown", "hungry", 0);
+INSERT INTO bears VALUES (7, "Wendy", 8, "F", "Brown", "sweet", 1);
+INSERT INTO bears VALUES (8, NULL, 9, "M", "Brown", "scary", 0);
+
+
+
